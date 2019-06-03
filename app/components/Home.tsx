94bc216065ef8aa5193b35bdb75e11fb47339d45
Jan Starzak <jan.starzak@gmail.com>
2019-06-04 01:40:29 +0200

    feat: initial commit

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 
 let styles = require('./Home.scss');
 
@@ -8,8 +7,6 @@ export default class Home extends React.Component {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
         </div>
       </div>
     );
--- app/main.development.js
+++ app/main.development.js
@@ -1,4 +1,7 @@
-const { app, BrowserWindow, Menu, shell } = require('electron');
+const { app, BrowserWindow, Menu, shell, nativeImage, Tray } = require('electron');
+const { getStreamDeckInfo, listStreamDecks, openStreamDeck } = require('elgato-stream-deck');
+const commander = require('commander');
+var path = require('path');
 
 let menu;
 let template;
@@ -16,257 +19,171 @@ if (process.env.NODE_ENV === 'development') {
   require('module').globalPaths.push(p); // eslint-disable-line
 }
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
+commander
+  .version('0.0.1', '-v, --version')
+  .usage('[OPTIONS]...')
+  .option('-u, --url', 'Open URL in Stream Deck browser')
+  .option('-s, --showWindow', 'Show rendering window')
+  .option('-l, --listDevices', 'List all Stream Deck devices')
+  .option('-d, --device', 'Connect to a device with a given serial number')
+  .option('--inspect', 'Show Developer Tools')
 
+if (process.defaultApp != true) {
+  process.argv.unshift(null)
+}
 
-const installExtensions = () => {
-  if (process.env.NODE_ENV === 'development') {
-    const installer = require('electron-devtools-installer'); // eslint-disable-line global-require
+commander.parse(process.argv)
 
-    const extensions = [
-      'REACT_DEVELOPER_TOOLS',
-      'REDUX_DEVTOOLS'
-    ];
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-    return Promise.all(extensions.map(name => installer.default(installer[name], forceDownload)));
+const url = commander.url || 'http://news.google.com/';
+const showWindow = !!commander.showWindow || false;
+const inspect = !!commander.inspect || false;
+const listDevices = !!commander.listDevices || false;
+const deviceSerial = commander.device || undefined;
+
+app.disableHardwareAcceleration();
+
+app.once('ready', () => {
+  const list = listStreamDecks();
+  if (list.length === 0) {
+    console.log('No StreamDeck found');
+    app.quit();
+  }
+  if (listDevices) {
+    console.log(JSON.stringify(list, undefined, 2))
+    app.quit();
+  }
+  let devicePath = undefined;
+  if (deviceSerial) {
+    const device = list.find(i => i.serialNumber === deviceSerial)
+    if (device) devicePath = device.path;
+  }
+  
+  const deck = openStreamDeck(devicePath || list[0].path);
+
+  tray = new Tray(nativeImage.createFromPath(path.join(__dirname, 'icon.png')));
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Quit', type: 'normal', click: () => {
+      app.quit();
+    } }
+  ])
+  tray.setToolTip(`Stream Deck Browser: ${deck.MODEL} ${deck.getSerialNumber()}`);
+  tray.setContextMenu(contextMenu);
+  
+  console.error('Connected to StreamDeck')
+  
+  deck.clearAllKeys();
+  
+  const PANEL_HEIGHT = (deck.KEY_ROWS * deck.ICON_SIZE);
+  const PANEL_WIDTH = (deck.KEY_COLUMNS * deck.ICON_SIZE);
+
+  
+  const STRIDE_BROWSER = 4
+  const STRIDE_DECK = 3
+  const frame = Buffer.alloc(PANEL_WIDTH * PANEL_HEIGHT * STRIDE_DECK);
+  console.log('width', PANEL_WIDTH, 'height', PANEL_HEIGHT, PANEL_WIDTH * PANEL_HEIGHT * STRIDE_DECK)
+
+  function copyBGRAtoRGB(source, target, targetStart, sourceStart, count) {
+    let i = 0;
+    for (i = 0; i < count; i++) {
+      target[(targetStart + i) * STRIDE_DECK + 0] = source[(sourceStart + i) * STRIDE_BROWSER + 2]
+      target[(targetStart + i) * STRIDE_DECK + 1] = source[(sourceStart + i) * STRIDE_BROWSER + 1]
+      target[(targetStart + i) * STRIDE_DECK + 2] = source[(sourceStart + i) * STRIDE_BROWSER + 0]
+    }
+  }
+  
+  function updateBitmap(rect, image) {
+    // console.log('Update');
+    const bmp = image.getBitmap ? image.getBitmap() : image;
+    // console.log(bmp.length)
+    const maxHeight = Math.min(rect.height, (PANEL_HEIGHT - rect.y));
+    const maxWidth = Math.min(rect.width, (PANEL_WIDTH - rect.x));
+    for (let i = 0; i < maxHeight; i++) {
+      const targetStart = ((rect.y + i) * PANEL_WIDTH + rect.x);
+      const sourceStart = (i * rect.width);
+      copyBGRAtoRGB(bmp, frame, targetStart, sourceStart, maxWidth);
+    }
+    deck.fillPanel(frame);
   }
 
-  return Promise.resolve([]);
-};
+  function keyIndexToXY(keyIndex) {
+    const y = (Math.floor(keyIndex / deck.KEY_COLUMNS) + 0.5) * deck.ICON_SIZE 
+    const x = (keyIndex % deck.KEY_COLUMNS + 0.5) * deck.ICON_SIZE
+    return { x, y }
+  }
 
-app.on('ready', () =>
-  installExtensions()
-  .then(() => {
-  mainWindow = new BrowserWindow({
+  let window = new BrowserWindow({
+    webPreferences: {
+      resizable: false,
+      enableRemoteModule: true,
+      backgroundThrottling: false,
+      nodeIntegration: true,
+    },
     show: false,
-    width: 1024,
-    height: 728
+    autoHideMenuBar: true,
+    width: PANEL_WIDTH,
+    height: PANEL_HEIGHT,
+    frame: false,
+    titleBarStyle: 'customButtonsOnHover', 
   });
 
-  mainWindow.loadURL(`file://${__dirname}/app.html`);
-
-  mainWindow.webContents.on('did-finish-load', () => {
-    mainWindow.show();
-    mainWindow.focus();
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-
-  if (process.env.NODE_ENV === 'development') {
-    mainWindow.openDevTools();
-    mainWindow.webContents.on('context-menu', (e, props) => {
-      const { x, y } = props;
-
-      Menu.buildFromTemplate([{
-        label: 'Inspect element',
-        click() {
-          mainWindow.inspectElement(x, y);
-        }
-      }]).popup(mainWindow);
+  window.loadURL(url);
+  if (inspect) window.webContents.toggleDevTools();
+  window.once('ready-to-show', () => {
+    if (showWindow) window.show();
+    console.log('Ready to Show');
+    console.log(window.getSize());
+    window.setSize(PANEL_WIDTH, PANEL_HEIGHT);
+    console.log(window.getSize());
+  })
+  window.once('closed', () => {
+    window = null;
+    deck.resetToLogo();
+  })
+  window.webContents.setFrameRate(4);
+  window.webContents.once('dom-ready', () => {
+    const requestSize = { x: 0, y: 0, width: PANEL_WIDTH, height: PANEL_HEIGHT };
+    
+    window.webContents.capturePage(requestSize, (image) => {
+      // console.log('Captured!');
+      const imgSize = image.getSize();
+      // console.log(imgSize)
+      updateBitmap({ x: 0, y: 0, width: imgSize.width, height: imgSize.height }, image);
     });
-  }
 
-  if (process.platform === 'darwin') {
-    template = [{
-      label: 'Electron',
-      submenu: [{
-        label: 'About ElectronReact',
-        selector: 'orderFrontStandardAboutPanel:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Services',
-        submenu: []
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Hide ElectronReact',
-        accelerator: 'Command+H',
-        selector: 'hide:'
-      }, {
-        label: 'Hide Others',
-        accelerator: 'Command+Shift+H',
-        selector: 'hideOtherApplications:'
-      }, {
-        label: 'Show All',
-        selector: 'unhideAllApplications:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click() {
-          app.quit();
-        }
-      }]
-    }, {
-      label: 'Edit',
-      submenu: [{
-        label: 'Undo',
-        accelerator: 'Command+Z',
-        selector: 'undo:'
-      }, {
-        label: 'Redo',
-        accelerator: 'Shift+Command+Z',
-        selector: 'redo:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Cut',
-        accelerator: 'Command+X',
-        selector: 'cut:'
-      }, {
-        label: 'Copy',
-        accelerator: 'Command+C',
-        selector: 'copy:'
-      }, {
-        label: 'Paste',
-        accelerator: 'Command+V',
-        selector: 'paste:'
-      }, {
-        label: 'Select All',
-        accelerator: 'Command+A',
-        selector: 'selectAll:'
-      }]
-    }, {
-      label: 'View',
-      submenu: (process.env.NODE_ENV === 'development') ? [{
-        label: 'Reload',
-        accelerator: 'Command+R',
-        click() {
-          mainWindow.webContents.reload();
-        }
-      }, {
-        label: 'Toggle Full Screen',
-        accelerator: 'Ctrl+Command+F',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
-        }
-      }, {
-        label: 'Toggle Developer Tools',
-        accelerator: 'Alt+Command+I',
-        click() {
-          mainWindow.toggleDevTools();
-        }
-      }] : [{
-        label: 'Toggle Full Screen',
-        accelerator: 'Ctrl+Command+F',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
-        }
-      }]
-    }, {
-      label: 'Window',
-      submenu: [{
-        label: 'Minimize',
-        accelerator: 'Command+M',
-        selector: 'performMiniaturize:'
-      }, {
-        label: 'Close',
-        accelerator: 'Command+W',
-        selector: 'performClose:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Bring All to Front',
-        selector: 'arrangeInFront:'
-      }]
-    }, {
-      label: 'Help',
-      submenu: [{
-        label: 'Learn More',
-        click() {
-          shell.openExternal('http://electron.atom.io');
-        }
-      }, {
-        label: 'Documentation',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme');
-        }
-      }, {
-        label: 'Community Discussions',
-        click() {
-          shell.openExternal('https://discuss.atom.io/c/electron');
-        }
-      }, {
-        label: 'Search Issues',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/issues');
-        }
-      }]
-    }];
+    window.webContents.beginFrameSubscription(true, (image, dirtyRect) => {
+      updateBitmap(dirtyRect, image);
+    });
 
-    menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-  } else {
-    template = [{
-      label: '&File',
-      submenu: [{
-        label: '&Open',
-        accelerator: 'Ctrl+O'
-      }, {
-        label: '&Close',
-        accelerator: 'Ctrl+W',
-        click() {
-          mainWindow.close();
-        }
-      }]
-    }, {
-      label: '&View',
-      submenu: (process.env.NODE_ENV === 'development') ? [{
-        label: '&Reload',
-        accelerator: 'Ctrl+R',
-        click() {
-          mainWindow.webContents.reload();
-        }
-      }, {
-        label: 'Toggle &Full Screen',
-        accelerator: 'F11',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
-        }
-      }, {
-        label: 'Toggle &Developer Tools',
-        accelerator: 'Alt+Ctrl+I',
-        click() {
-          mainWindow.toggleDevTools();
-        }
-      }] : [{
-        label: 'Toggle &Full Screen',
-        accelerator: 'F11',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
-        }
-      }]
-    }, {
-      label: 'Help',
-      submenu: [{
-        label: 'Learn More',
-        click() {
-          shell.openExternal('http://electron.atom.io');
-        }
-      }, {
-        label: 'Documentation',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme');
-        }
-      }, {
-        label: 'Community Discussions',
-        click() {
-          shell.openExternal('https://discuss.atom.io/c/electron');
-        }
-      }, {
-        label: 'Search Issues',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/issues');
-        }
-      }]
-    }];
-    menu = Menu.buildFromTemplate(template);
-    mainWindow.setMenu(menu);
-  }
-}));
+    deck.on('down', (keyIndex) => {
+      const { x, y } = keyIndexToXY(keyIndex);
+      window.webContents.sendInputEvent({
+        type: 'mouseMove',
+        x,
+        y
+      });
+      window.webContents.sendInputEvent({
+        type: 'mouseDown',
+        x,
+        y,
+        button: 'left',
+        clickCount: 1
+      });
+    });
+
+    deck.on('up', (keyIndex) => {
+      const { x, y } = keyIndexToXY(keyIndex);
+      window.webContents.sendInputEvent({
+        type: 'mouseUp',
+        modifiers: ['isKeypad'],
+        x,
+        y,
+        button: 'left',
+        clickCount: 1
+      });
+    });
+  });
+
+  deck.on('error', error => {
+    console.error(error);
+  })
+});
