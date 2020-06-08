64c22e1dc074d14c6bfd3f3be4c6e452bb4fab56
Jan Starzak <jan.starzak@gmail.com>
2020-06-08 14:08:19 +0200

    feat: survive streamdeck disconnection

--- app/main.development.ts
+++ app/main.development.ts
@@ -1,6 +1,6 @@
 /// <reference path="./api/electron.d.ts" />
 
-import { app, BrowserWindow, Menu, nativeImage, Tray, NativeImage, ipcMain, IpcAsyncMessageEvent, screen } from "electron"
+import { app, BrowserWindow, Menu, nativeImage, Tray, NativeImage, ipcMain, IpcAsyncMessageEvent, screen, Rectangle } from "electron"
 import { listStreamDecks, openStreamDeck, StreamDeck, StreamDeckDeviceInfo } from 'elgato-stream-deck'
 import { CommandMessage, CommandMessageType } from './api/CommandMessage'
 import * as commander from 'commander'
@@ -43,6 +43,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 const DEFAULT_BRIGHTNESS = 75
 const APP_SETTINGS = path.join(__dirname, 'app.html')
+const RECONNECT_INTERVAL = 3000
 
 let url = commander.url || DEFAULT_URL
 let currentUrl = url
@@ -95,6 +96,21 @@ function showAbout() {
 	})
 }
 
+function reconnectToStreamDeck(deviceSerial?: string): StreamDeck | undefined {
+	list = listStreamDecks()
+	let devicePath = undefined
+	if (deviceSerial) {
+		const device = list.find(i => i.serialNumber === deviceSerial)
+		if (device) {
+			devicePath = device.path
+		} else {
+			return
+		}
+	}
+	
+	return openStreamDeck(devicePath || list[0].path)
+}
+
 app.once('ready', () => {
 	url = commander.url || settings.get('url', DEFAULT_URL)
 	currentUrl = url
@@ -145,40 +161,46 @@ app.once('ready', () => {
 	
 	deck.clearAllKeys()
 	
-	const PANEL_HEIGHT = (deck.KEY_ROWS * deck.ICON_SIZE)
-	const PANEL_WIDTH = (deck.KEY_COLUMNS * deck.ICON_SIZE)
+	let PANEL_HEIGHT = (deck.KEY_ROWS * deck.ICON_SIZE)
+	let PANEL_WIDTH = (deck.KEY_COLUMNS * deck.ICON_SIZE)
 
 	
 	const STRIDE_BROWSER = 4
 	const STRIDE_DECK = 3
-	const frame = Buffer.alloc(PANEL_WIDTH * PANEL_HEIGHT * STRIDE_DECK)
+	let frame = Buffer.alloc(PANEL_WIDTH * PANEL_HEIGHT * STRIDE_DECK)
 	// console.log('width', PANEL_WIDTH, 'height', PANEL_HEIGHT, PANEL_WIDTH * PANEL_HEIGHT * STRIDE_DECK)
 
-	function copyBGRAtoRGB(source: Buffer, target: Buffer, targetStart: number, sourceStart: number, count: number) {
-		let i = 0;
-		for (i = 0; i < count; i++) {
-			target[(targetStart + i) * STRIDE_DECK + 0] = source[(sourceStart + i) * STRIDE_BROWSER + 2]
-			target[(targetStart + i) * STRIDE_DECK + 1] = source[(sourceStart + i) * STRIDE_BROWSER + 1]
-			target[(targetStart + i) * STRIDE_DECK + 2] = source[(sourceStart + i) * STRIDE_BROWSER + 0]
-		}
+	function rebuildPanelSettings(deck: StreamDeck) {
+		PANEL_HEIGHT = (deck.KEY_ROWS * deck.ICON_SIZE)
+		PANEL_WIDTH = (deck.KEY_COLUMNS * deck.ICON_SIZE)
+
+		frame = Buffer.alloc(PANEL_WIDTH * PANEL_HEIGHT * STRIDE_DECK)
 	}
 	
 	function updateBitmap(rect: Electron.Rectangle, image: NativeImage | Buffer) {
 		// console.log('Update');
-		const bmp = (image as NativeImage).getBitmap ? (image as NativeImage).getBitmap() : (image as Buffer);
+		const srcBmp = (image as NativeImage).getBitmap ? (image as NativeImage).getBitmap() : (image as Buffer)
+		const targetBmp = frame
 		// console.log(bmp.length)
 		const maxHeight = Math.min(rect.height, (PANEL_HEIGHT - rect.y))
 		const maxWidth = Math.min(rect.width, (PANEL_WIDTH - rect.x))
-		for (let i = 0; i < maxHeight; i++) {
-			const targetStart = ((rect.y + i) * PANEL_WIDTH + rect.x)
-			const sourceStart = (i * rect.width)
-			copyBGRAtoRGB(bmp, frame, targetStart, sourceStart, maxWidth)
+
+		// this is somewhat weird.
+		rect.y = 0
+		rect.x = 0
+
+		for (let y = 0; y < maxHeight; y++) {
+			for (let x = 0; x < maxWidth; x++) {
+				targetBmp[((rect.y * rect.width) + rect.x + (y * rect.width) + x) * STRIDE_DECK + 0] = srcBmp[((y * rect.width) + x) * STRIDE_BROWSER + 2]
+				targetBmp[((rect.y * rect.width) + rect.x + (y * rect.width) + x) * STRIDE_DECK + 1] = srcBmp[((y * rect.width) + x) * STRIDE_BROWSER + 1]
+				targetBmp[((rect.y * rect.width) + rect.x + (y * rect.width) + x) * STRIDE_DECK + 2] = srcBmp[((y * rect.width) + x) * STRIDE_BROWSER + 0]
+			}
 		}
+
 		try {
-			if (deck) deck.fillPanel(frame);
+			if (deck) deck.fillPanel(frame)
 		} catch (e) {
-			deck = undefined;
-			app.quit();
+			deck = undefined
 		}
 	}
 
@@ -198,68 +220,16 @@ app.once('ready', () => {
 		deck.setBrightness(Math.max(0, Math.min(100, level)))
 	}
 
-	let window: BrowserWindow | undefined = new BrowserWindow({
-		webPreferences: {
-			enableRemoteModule: false,
-			backgroundThrottling: false,
-			nodeIntegration: false,
-			allowRunningInsecureContent: true,
-			contextIsolation: true
-		},
-		resizable: false,
-		show: false,
-		autoHideMenuBar: true,
-		width: PANEL_WIDTH,
-		height: PANEL_HEIGHT,
-		frame: false
-	});
-
-	openInDeck(currentUrl)
-	if (inspect) window.webContents.toggleDevTools()
-	window.once('ready-to-show', () => {
-		if (!window) return
-		if (showWindow) window.show()
-		window.setSize(PANEL_WIDTH, PANEL_HEIGHT)
-	})
-	window.once('closed', () => {
-		window = undefined
-		if (deck) deck.resetToLogo()
-	})
-	window.webContents.on('did-start-navigation', (event, url, isInPlace, isMainFrame) => {
-		if (isMainFrame) {
-			currentUrl = url
-		}
-
-		if (aboutWindow) {
-			aboutWindow.webContents.send('asynchronous-message', {
-				type: CommandMessageType.SET_SETTINGS,
-				settings: {
-					currentUrl
-				}
+	function refreshFullDeck(requestSize: Rectangle) {
+		if (window) {
+			window.webContents.capturePage(requestSize, (image) => {
+				const imgSize = image.getSize()
+				updateBitmap({ x: 0, y: 0, width: imgSize.width, height: imgSize.height }, image)
 			})
 		}
-	})
-	window.webContents.on('did-finish-load', () => {
-		window!.webContents.insertCSS('html, body { overflow: hidden !important; }')
-	})
-	window.webContents.once('dom-ready', () => {
-		if (!window) return
-		const requestSize = { x: 0, y: 0, width: PANEL_WIDTH, height: PANEL_HEIGHT }
-		
-		window.webContents.capturePage(requestSize, (image) => {
-			const imgSize = image.getSize()
-			updateBitmap({ x: 0, y: 0, width: imgSize.width, height: imgSize.height }, image)
-		})
+	}
 
-		window.webContents.on('paint', (e, dirtyRect, image) => {
-			updateBitmap(dirtyRect, image)
-		})
-
-		if (!deck) {
-			app.quit()
-			return;
-		}
-
+	function attachEvents(deck: StreamDeck, window: BrowserWindow) {
 		deck.on('down', (keyIndex: number) => {
 			const { x, y } = keyIndexToXY(keyIndex);
 			if (!window) return
@@ -288,10 +258,108 @@ app.once('ready', () => {
 				clickCount: 1
 			} as any as Electron.Event)
 		})
+	}
+
+	let window: BrowserWindow | undefined = new BrowserWindow({
+		webPreferences: {
+			enableRemoteModule: false,
+			backgroundThrottling: false,
+			nodeIntegration: false,
+			allowRunningInsecureContent: true,
+			contextIsolation: true
+		},
+		resizable: false,
+		show: false,
+		autoHideMenuBar: true,
+		width: PANEL_WIDTH,
+		height: PANEL_HEIGHT,
+		frame: false
+	});
+
+	openInDeck(currentUrl)
+	if (inspect) window.webContents.toggleDevTools()
+	window.once('ready-to-show', () => {
+		if (!window) return
+		if (showWindow) window.show()
+		window.setSize(PANEL_WIDTH, PANEL_HEIGHT)
+	})
+	window.once('closed', () => {
+		window = undefined
+		try {
+			if (deck) deck.resetToLogo()
+		} catch(e) {
+			console.error(e)
+		}
+	})
+	window.webContents.on('did-start-navigation', (event, url, isInPlace, isMainFrame) => {
+		if (isMainFrame) {
+			currentUrl = url
+		}
+
+		if (aboutWindow) {
+			aboutWindow.webContents.send('asynchronous-message', {
+				type: CommandMessageType.SET_SETTINGS,
+				settings: {
+					currentUrl
+				}
+			})
+		}
+	})
+	window.webContents.on('did-finish-load', () => {
+		window!.webContents.insertCSS('html, body { overflow: hidden !important; }')
+	})
+	window.webContents.once('dom-ready', () => {
+		if (!window) return
+		const requestSize = { x: 0, y: 0, width: PANEL_WIDTH, height: PANEL_HEIGHT }
+		
+		refreshFullDeck(requestSize)
+
+		window.webContents.on('paint', (e, dirtyRect, image) => {
+			updateBitmap(dirtyRect, image)
+		})
+
+		window.webContents.beginFrameSubscription((image, dirtyRect) => {
+			console.log(dirtyRect)
+			updateBitmap(dirtyRect, image)
+		})
+
+		if (!deck) {
+			app.quit()
+			return;
+		}
+
+		attachEvents(deck, window)
 	})
 
+	let reconnecting = false
+	function pollToReconnect() {
+		if (!deck) {
+			reconnecting = true
+			console.log('Retrying...')
+			deck = reconnectToStreamDeck(deviceSerial)
+			if (!deck) {
+				setTimeout(pollToReconnect, RECONNECT_INTERVAL)
+			} else {
+				console.log(`Reconnected to Stream Deck, Model: ${deck.MODEL}, S/N: ${deck.getSerialNumber()}`)
+				rebuildPanelSettings(deck)
+				const requestSize = { x: 0, y: 0, width: PANEL_WIDTH, height: PANEL_HEIGHT }
+				refreshFullDeck(requestSize)
+				if (window) attachEvents(deck, window)
+				reconnecting = false
+			}
+		}
+	}
+
 	deck.on('error', (error: any) => {
-		console.error(error);
+		if (error && error.message.match(/could not read from HID device/i)) {
+			console.log(`Connection to Stream Deck lost, polling in ${RECONNECT_INTERVAL/1000}s...`)
+			deck = undefined
+			if (!reconnecting) {
+				setTimeout(pollToReconnect, RECONNECT_INTERVAL)
+			}
+		} else {
+			console.error(error);
+		}
 	})
 
 	ipcMain.on('asynchronous-message', (event: IpcAsyncMessageEvent, arg: CommandMessage) => {
